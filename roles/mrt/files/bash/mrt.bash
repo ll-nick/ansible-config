82b8aefd665b2b9a14b7f40a3d9d8880a7d27eec
Nick Le Large <nick.lelarge@kit.edu>
2025-02-03 11:20:50 +0100

    Remove codews alias

--- roles/mrt/files/bash/mrt.bash
+++ roles/mrt/files/bash/mrt.bash
@@ -1,7 +1,6 @@
 source /opt/mrtsoftware/setup.bash
 source /opt/mrtros/setup.bash
 
-alias codews='code $(mrt catkin locate)'
 alias kitvpn='sudo openvpn --config ~/Documents/kit.ovpn'
 alias mensa='kit-mensa-cli'
 alias rf='rosbag_fancy'
